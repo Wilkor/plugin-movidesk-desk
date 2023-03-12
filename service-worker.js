e0c389b8832ad47f3927c3371ae5c8ebbc98c23a
Wilkor <wilkorguitarrista@msn.com>
2023-03-12 20:43:25 -0300

    Melhorias
    Setando configuração direto no banco

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.5422ebee548a905bcfec355896982661.js"
+  "./precache-manifest.25342ca76d48f747d949541e63c9351c.js"
 );
 
 self.addEventListener('message', (event) => {
