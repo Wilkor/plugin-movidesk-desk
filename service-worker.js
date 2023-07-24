6c679e56916d348a34fe42b513371fc22ce22ea9
Wilkor <wilkorguitarrista@msn.com>
2023-07-24 10:00:09 -0300

    fix
    Ajuste na validação da key no movidesk

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ca3204a9de00a7297c17a0cc30baff02.js"
+  "./precache-manifest.a80f109dbbe08677f960fe5ff2de63a5.js"
 );
 
 self.addEventListener('message', (event) => {
