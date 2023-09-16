77e0697008b1a68e9fb9a33c2f233672e52c2a8c
Wilkor <wilkorguitarrista@msn.com>
2023-09-16 12:59:20 -0300

    melhorias
    adicionando serviços do movidesk para selecionar o serviço

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a80f109dbbe08677f960fe5ff2de63a5.js"
+  "./precache-manifest.a4add3c9d21656e028b8550b849c15c2.js"
 );
 
 self.addEventListener('message', (event) => {
